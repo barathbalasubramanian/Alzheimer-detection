377e1084f0112801231e59c620f1edc58ea43ef8
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-08-02 14:36:22 +0530

    Receiving data from server

--- AlzheimerDetection/settings.py
+++ AlzheimerDetection/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#3*shu-=mmnxs5diq@g2a99euo9h$4a+i7eqdf^16k5!q7)y#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,8 @@
-from ultralytics import YOLO
+# from ultralytics import YOLO
 from PIL import Image
 import cv2
 import io
+import requests , json
 
 from django.shortcuts import render
 
@@ -13,24 +14,50 @@ def index(request) :
 
     if request.method == "POST" :
         if 'file' in request.POST :
+
             print('File ..')
             myfile = request.FILES['fileUpload']
             content = myfile.read()
             image = Image.open(io.BytesIO(content))
             image.save('frontend\static\image\scan_image.png')
 
-            model = YOLO(r'models\weights.pt')
-            im2 = cv2.imread(r"frontend\static\image\scan_image.png")
-            results = model.predict(source=im2)
-            pre = results[0].boxes.cls.tolist()
-            print(classes[int(pre[0])])
+            url = 'http://localhost:3000/upload'
 
-            cv2.imwrite('frontend\static\image\save.png',results[0].plot())
+            image_path = 'frontend\static\image\scan_image.png'  
+
+            with open(image_path, 'rb') as image:
+                files = {'fileUpload': image}
+                response = requests.post(url, files=files)
+                print(response.status_code)
+                print(response.headers)
+
+                if response.status_code == 200:
+                    content_disposition = response.headers.get('Content-Disposition')
+                    if content_disposition:
+                        filename = content_disposition
+                        print('Filename: ', filename)
+
+                    with open('frontend\static\image\save1.png', 'wb') as f:
+                        f.write(response.content)
+                    print("File downloaded successfully")
+                else:
+                    print("Failed to download the file")
 
             img = [0]
             context = {
-                'ref' : img
+                'ref' : img,
+                'detect': filename
             }
             return render(request, 'index.html' , context)
 
     return render(request, 'index.html')
+
+
+# model = YOLO(r'models\weights.pt')
+# im2 = cv2.imread(r"frontend\static\image\scan_image.png")
+# results = model.predict(source=im2)
+# print(results)
+# print(results[0],'------')
+# pre = results[0].boxes.cls.tolist()
+# print(classes[int(pre[0])])
+# cv2.imwrite('frontend\static\image\save.png',results.plot())
