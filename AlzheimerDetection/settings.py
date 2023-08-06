3773d7964df7713e22b8668e70ec210ed576f9ac
barathbalasubramanian <barathkumar.b2411@gmail.com>
2023-08-06 10:45:14 +0530

    Dockerfile

--- AlzheimerDetection/settings.py
+++ AlzheimerDetection/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#3*shu-=mmnxs5diq@g2a99euo9h$4a+i7eqdf^16k5!q7)y#-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
